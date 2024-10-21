3b0d00d4409b6118a92311180e3a1dee0aed29ee
msnehaha <snehasth11@gmail.com>
2024-10-22 00:02:41 +0545

    Add files via upload

---
+++ auth/sign-in/index.js
@@ -0,0 +1,185 @@
+import { router, useNavigation, useRouter } from "expo-router";
+import React, { useEffect, useState } from "react";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  ToastAndroid,
+} from "react-native";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./../../../configs/FirebaseConfig";
+
+export default function SignInScreen() {
+  const navigation = useNavigation();
+  const router = useRouter();
+
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  const onSignIn = () => {
+    console.log("clicked");
+    if (!email || !password) {
+      ToastAndroid.show("Please enter all the details", ToastAndroid.LONG);
+      return;
+    }
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        if (errorCode === "auth/invalid-credential") {
+          ToastAndroid.show(
+            "Please enter correct credentials",
+            ToastAndroid.LONG
+          );
+          return;
+        }
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => router.back()} style={styles.arrowBtn}>
+        <AntDesign name="arrowleft" size={24} color="black" />
+      </TouchableOpacity>
+
+      {/* Welcome Text */}
+      <Text style={styles.welcomeText}>Welcome User 👋</Text>
+
+      {/* Email Input */}
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Email"
+        keyboardType="email-address"
+        onChangeText={(value) => setEmail(value)}
+      />
+
+      {/* Password Input */}
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Password"
+        secureTextEntry
+        onChangeText={(value) => setPassword(value)}
+      />
+
+      {/* Sign In Button */}
+      <TouchableOpacity style={styles.signInButton} onPress={onSignIn}>
+        <Text style={styles.signInText}>Sign In</Text>
+      </TouchableOpacity>
+
+      {/* Google Sign In Button */}
+      <TouchableOpacity style={styles.googleButton}>
+        <Image
+          source={{
+            uri: "https://static-00.iconduck.com/assets.00/google-icon-512x512-wk1c10qc.png",
+          }} // Placeholder for Google logo
+          style={styles.googleLogo}
+        />
+        <Text style={styles.googleText}>Sign in with Google</Text>
+      </TouchableOpacity>
+
+      {/* Divider Line */}
+      <View style={styles.divider} />
+
+      {/* Create Account Button */}
+      <TouchableOpacity
+        style={styles.createAccountButton}
+        onPress={() => router.replace("auth/sign-up")}
+      >
+        <Text style={styles.createAccountText}>Create Account</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  welcomeText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 30,
+  },
+  arrowBtn: {
+    position: "absolute",
+    top: 40,
+    left: 20,
+  },
+  input: {
+    width: "100%",
+    padding: 15,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: "#ccc",
+    marginBottom: 15,
+  },
+  signInButton: {
+    width: "100%",
+    backgroundColor: "black",
+    padding: 15,
+    borderRadius: 8,
+    alignItems: "center",
+    marginBottom: 15,
+  },
+  signInText: {
+    color: "white",
+    fontSize: 16,
+  },
+  googleButton: {
+    width: "100%",
+    backgroundColor: "#4285F4",
+    padding: 10,
+    borderRadius: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  googleLogo: {
+    width: 30,
+    height: 30,
+    marginRight: 10,
+  },
+  googleText: {
+    color: "white",
+    fontSize: 16,
+  },
+  divider: {
+    width: "100%",
+    height: 1,
+    backgroundColor: "#ccc", // Divider color
+    marginVertical: 30, // Space between the buttons
+  },
+  createAccountButton: {
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 15,
+    width: "100%",
+    alignItems: "center",
+  },
+  createAccountText: {
+    color: "black",
+    fontSize: 16,
+  },
+});
---
+++ auth/sign-up/index.js
@@ -0,0 +1,170 @@
+import React, { useEffect, useState } from "react";
+import { router, useNavigation, useRouter } from "expo-router";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  ToastAndroid,
+} from "react-native";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./../../../configs/FirebaseConfig";
+
+export default function CreateAccountScreen() {
+  const navigation = useNavigation();
+  const router = useRouter();
+
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [fullName, setFullName] = useState();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  const onCreateAccount = () => {
+    if (!email || !password || !fullName) {
+      ToastAndroid.show("Please enter all the details", ToastAndroid.LONG);
+      return;
+    }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        // ..
+      });
+  };
+  return (
+    <View style={styles.container}>
+      {/* Title */}
+      <TouchableOpacity onPress={() => router.back()} style={styles.arrowBtn}>
+        <AntDesign name="arrowleft" size={24} color="black" />
+      </TouchableOpacity>
+      <Text style={styles.title}>Create New Account</Text>
+
+      {/* Full Name Input */}
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Full Name"
+        placeholderTextColor="#A9A9A9"
+        onChangeText={(value) => setFullName(value)}
+      />
+
+      {/* Email Input */}
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Email"
+        placeholderTextColor="#A9A9A9"
+        keyboardType="email-address"
+        onChangeText={(value) => setEmail(value)}
+      />
+
+      {/* Password Input */}
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Password"
+        placeholderTextColor="#A9A9A9"
+        secureTextEntry={true}
+        onChangeText={(value) => setPassword(value)}
+      />
+
+      {/* Sign In Button */}
+      <TouchableOpacity style={styles.signInButton} onPress={onCreateAccount}>
+        <Text style={styles.signInButtonText}>Sign In</Text>
+      </TouchableOpacity>
+
+      {/* OR Line */}
+      <Text style={styles.orText}>OR</Text>
+
+      {/* Google Sign-In Button */}
+      <TouchableOpacity style={styles.googleButton}>
+        <Image
+          source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png",
+          }}
+          style={styles.googleIcon}
+        />
+        <Text style={styles.googleButtonText}>Sign in with Google</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 30,
+  },
+  arrowBtn: {
+    position: "absolute",
+    top: 40,
+    left: 20,
+  },
+  input: {
+    width: "100%",
+    height: 50,
+    borderColor: "#D3D3D3",
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    backgroundColor: "#F9F9F9",
+  },
+  signInButton: {
+    backgroundColor: "#000",
+    paddingVertical: 15,
+    paddingHorizontal: 80,
+    borderRadius: 10,
+    marginTop: 20,
+  },
+  signInButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  orText: {
+    marginVertical: 20,
+    color: "#A9A9A9",
+    fontSize: 16,
+  },
+  googleButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderColor: "#4285F4",
+    borderWidth: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+  },
+  googleIcon: {
+    width: 25,
+    height: 25,
+    marginRight: 10,
+  },
+  googleButtonText: {
+    color: "#4285F4",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
